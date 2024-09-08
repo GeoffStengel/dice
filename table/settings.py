415278e4dc4010128b56bb4e584d17d4ffc19fcb
unknown <geoffsglass@hotmail.com>
2024-09-08 11:39:54 -0800

    testing more

--- table/settings.py
+++ table/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = os.environ.get('DEBUG', 'True') == 'True'  # Ensure DEBUG is a boolean
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'  # Ensure DEBUG is a boolean
 ADMIN_URL = os.environ.get('ADMIN')
 #We Added This For Your .env Files To Stay Secure
 
