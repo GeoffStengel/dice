c6beadac47efa9fe85f9617f8924ecd05912849d
unknown <geoffsglass@hotmail.com>
2024-09-08 16:15:04 -0800

    Added more path for STATICFILES_DIRS

--- table/settings.py
+++ table/settings.py
@@ -140,7 +140,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 #original#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'home/static'),
 ]
 
 django_heroku.settings(locals())
